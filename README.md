45f4551b0740b7b1522a21b4b2fa6e8d733c7729
LylyG <lgalarza@spoonflower.com>
2016-03-09 10:10:29 -0500

    Wednesday Challenge 3-09

---
+++ README.md
@@ -0,0 +1,2 @@
+##Reflection on strong parameters
+I think requiring strong params is great, but limited approach to preventing mass assignment. Since this particular form has a lot of checkboxes, I'm not sure how much good strong params will do here. 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,12 @@
 class HomeController < ApplicationController
+  before_action :form
 
   def form
-    
+    @home = Home.new
   end
+
+  private def home_params
+        params.require(:home).permit(:name, :puppies, :rainbows, :chocolate, :cash)
+      end
 
 end
